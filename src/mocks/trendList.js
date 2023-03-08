d0c683bdec34b49b9d941db5c811348534c988d9
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-03-08 16:39:32 +0100

    updated

--- src/mocks/trendList.js
+++ src/mocks/trendList.js
@@ -47,69 +47,6 @@ const trendList = [
     trending: "world",
     tweetNumb: "70k",
   },
-  //   {
-  //     id: 9,
-  //     body: "I knew you could do it!",
-  //     postId: 64,
-  //     user: {
-  //       id: 31,
-  //       username: "smargiottau",
-  //     },
-  //   },
-  //   {
-  //     id: 10,
-  //     body: "Wonderful ideas!",
-  //     postId: 4,
-  //     user: {
-  //       id: 35,
-  //       username: "mbrooksbanky",
-  //     },
-  //   },
-  //   {
-  //     id: 11,
-  //     body: "It was a pleasure to grade this!",
-  //     postId: 2,
-  //     user: {
-  //       id: 68,
-  //       username: "rstrettle1v",
-  //     },
-  //   },
-  //   {
-  //     id: 12,
-  //     body: "Keep up the incredible work!",
-  //     postId: 50,
-  //     user: {
-  //       id: 77,
-  //       username: "rkingswood24",
-  //     },
-  //   },
-  //   {
-  //     id: 13,
-  //     body: "My goodness, how impressive!",
-  //     postId: 37,
-  //     user: {
-  //       id: 28,
-  //       username: "xisherwoodr",
-  //     },
-  //   },
-  //   {
-  //     id: 14,
-  //     body: "You’re showing inventive ideas!",
-  //     postId: 30,
-  //     user: {
-  //       id: 57,
-  //       username: "bpickering1k",
-  //     },
-  //   },
-  //   {
-  //     id: 15,
-  //     body: "You’ve shown so much growth!",
-  //     postId: 44,
-  //     user: {
-  //       id: 76,
-  //       username: "cgaber23",
-  //     },
-  //   },
 ];
 
 export default trendList;
